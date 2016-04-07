cbe66aa10b9d93192be1fcec9c9619cc7812a16b
Everett Kropf <ehkropf@gmail.com>
2016-04-07 13:27:13 +0100

    Updated example.

--- example.m
+++ example.m
@@ -1,20 +1,82 @@
 %% Examples.
+% This example file requires the conformal mapping toolkit (CMT).
+
 clear
 
 
 %%
-% A bounded unit domain to work with.
+% Unbounded circle domain. Let's also have a grid to plot off.
 
-dv = [
-    -0.2517+0.3129i
-    0.2307-0.4667i];
-qv = [
-    0.2377
-    0.1557];
+sv = [
+    -0.48951-1.7395i
+    -1.6608+1.4423i
+    2.5874+0.16608i];
+rv = [
+    1.2457
+    0.93902
+    0.932];
+Om = circleRegion(sv, rv);
 
-D = unitDomain(dv, qv);
+ax = plotbox(Om, 1.2);
+res = 100;
+[zg, zeta] = meshgrid(linspace(ax(1), ax(2), res), ...
+    linspace(ax(3), ax(4), res));
+zg = complex(zg, zeta);
+for j = 1:numel(sv)
+    zg(abs(zg - sv(j)) <= rv(j)+eps(max(abs(sv)))) = nan;
+end
+
+
+%%
+% Equivalent bounded unit domain.
+
+zeta = mobius(0, rv(1), 1, -sv(1));
+D = zeta(Om);
+dv = D.centers(2:end);
+qv = D.radii(2:end);
+beta = pole(inv(zeta));
+
+D = unitDomain(dv, qv, beta);
+
+
+%%
+% Circulation.
+
+circ = circulation(2, -1);
+circn = circulationNoNet(1, 2, -1);
+
+
+%%
+% Point vortices.
+
+av = [
+    0.25641+0.38313i
+    -1.9915-0.58025i
+    0.7488+2.5454i
+    2.1618-1.3938i];
+gv = [1, -1, 1, 1];
+
+pv = pointVortex(zeta(av), gv);
+pvn = pointVortexNoNet(zeta(av), gv);
 
 
 %%
 
-W = potential(D);
+W = potential(D, pv, circ);
+
+wt = W(0.5+0.1i);
+
+
+%%
+% Plot streamlines.
+
+wg = W(zeta(zg));
+
+figure(1), clf
+contour(real(zg), imag(zg), imag(wg), 20, 'linecolor', [0.2081, 0.1663, 0.5292])
+hold on
+fill(inv(Om))
+plot(Om)
+plot(av, 'k.')
+hold off
+set(gca, 'dataaspectratio', [1, 1, 1])
