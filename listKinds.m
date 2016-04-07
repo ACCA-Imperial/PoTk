48ae7d4d3cb641d73ed04f810d363090258cdbb8
Everett Kropf <ehkropf@gmail.com>
2016-04-07 17:05:49 +0100

    Add function to list the potential kind classes.

---
+++ listKinds.m
@@ -0,0 +1,52 @@
+function listKinds()
+%listKinds lists potentialKind objects.
+%
+%  listKinds()
+%    Lists all objects of class "potentialKind" in PoTk to the command
+%    window.
+%
+%See also potential, potentialKind.
+
+% Everett Kropf, 2016
+% 
+% This file is part of the Potential Toolkit (PoTk).
+% 
+% PoTk is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% PoTk is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with PoTk.  If not, see <http://www.gnu.org/licenses/>.
+
+podir = fileparts(mfilename('fullpath'));
+curdir = pwd;
+
+cd(podir)
+
+dinfo = what;
+potKind = {};
+for i = 1:numel(dinfo.classes)
+    cname = dinfo.classes{i};
+    if meta.class.fromName(cname).Abstract
+        continue
+    end
+    obj = eval(cname);
+    if isa(obj, 'potentialKind')
+        potKind{end+1} = cname; %#ok<AGROW>
+    end
+end
+
+fprintf('Kinds of potential contributions in PoTk:\n')
+for i = 1:numel(potKind)
+    name = potKind{i};
+    fprintf('  %s\n', name)
+end
+fprintf('See help text for each for details.\n')
+
+cd(curdir)
