84e403544bce099c1df68a38748b3de7a4f0ac79
Everett Kropf <ehkropf@gmail.com>
2016-05-24 15:48:15 +0100

    Record some scratch work.

---
+++ tex/pub_latexRepMJ.m
@@ -0,0 +1,31 @@
+%%
+% The analytic expression for the potential is
+%
+% \[ W(\zeta) = W_1(\zeta) + W_2(\zeta) \]
+%
+% \[ W_1(\zeta) = \sum_{k=1}^N 
+% \gamma_k G_0(\zeta,\alpha_k,\overline{\alpha_k})
+% \qquad \mathrm{(point\; vortices)} \]
+%
+% \[ W_2(\zeta) = 2\pi U_f b\mathrm{i} \left( e^{\mathrm{i}\chi}
+% \frac{\partial}{\partial\overline{\alpha}}
+% G_0(\zeta,\alpha,\overline{\alpha}) - e^{-\mathrm{i}\chi}
+% \frac{\partial}{\partial\alpha} G_0(\zeta,\alpha,\overline{\alpha})
+% \right) \qquad \mathrm{(uniform\; flow)} \]
+%
+% where we define
+%
+% the Schottky-Klein prime function \(\omega(\zeta,\alpha)\)
+%
+% \[ G_0(\zeta,\alpha,\overline{\alpha}) = \frac{1}{2\pi\mathrm{i}}
+% \log \left[ \frac{\omega(\zeta,\alpha)}{|a| 
+% \omega(\zeta,1/\overline{\alpha})} \right] \]
+%
+% the point vortex locations \(\alpha_k\) with strength \(\gamma_k\)
+%
+% the uniform flow strength \(U_f\) at angle \(\chi\)
+%
+% and the constant \(b\) given by the form of the map \(z(\zeta)\) from the
+% unit domain to the physical domain as \(\zeta\to\beta\) such that
+%
+% \[ z(\zeta) = \frac{b}{\zeta - \beta} + \mathrm{analytic\; part} \]
