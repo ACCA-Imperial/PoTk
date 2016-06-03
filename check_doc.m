28824cb8c39650547338874793a78559e1200783
Everett Kropf <ehkropf@gmail.com>
2016-06-03 16:06:50 +0100

    Update check doc.

--- check_doc.m
+++ check_doc.m
@@ -4,30 +4,30 @@ clear
 
 %%
 
-% dv = [
-%   -0.2517+0.3129i
-%    0.2307-0.4667i];
-% qv = [
-%   0.2377
-%   0.1557];
-% D = unitDomain(dv, qv, 0);
+dv = [
+  -0.2517+0.3129i
+   0.2307-0.4667i];
+qv = [
+  0.2377
+  0.1557];
+D = unitDomain(dv, qv, 0);
 
-D = unitDomain(0, 0.25, -0.75);
+% D = unitDomain(0, 0.25, -0.75);
 
-% av = [
-%     -0.087464-0.62274i
-%     0.32536+0.50379i
-%     -0.42332+0.74169i];
-% pv = pointVortexNoNet(av, ones(size(av)));
+av = [
+    -0.087464-0.62274i
+    0.32536+0.50379i
+    -0.42332+0.74169i];
+pv = pointVortexNoNet(av, ones(size(av)));
 % pv = pointVortexNoNet(0.7, 1);
 
-% uf = uniformFlow(1, 0);
+uf = uniformFlow(1, 0);
 
-% C = circulationNoNet([1, 1, -1]);
-C = circulation(1);
+C = circulationNoNet([1, 1, -1]);
+% C = circulation([1, -1]);
 
 
 %%
 
-W = potential(D, C);
+W = potential(D, C, pv, uf);
 podoc(W)
