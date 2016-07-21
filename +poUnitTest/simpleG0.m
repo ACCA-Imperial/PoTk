5e9e1857343c4fc847a161d82c3c14be45976042
Everett Kropf <ehkropf@gmail.com>
2016-07-21 17:22:25 +0100

    Provide simply connected G0 for testing.

---
+++ +poUnitTest/simpleG0.m
@@ -0,0 +1,25 @@
+function v = simpleG0(z, a)
+%poUnitTest provides the simple Green's function G0 for unit tests.
+
+% Everett Kropf, 2016
+% 
+% This file is part of the Potential Toolkit (PoTk).
+% 
+% PoTk is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% PoTk is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with PoTk.  If not, see <http://www.gnu.org/licenses/>.
+
+if a == 0
+    v = log(z)/2i/pi;
+else
+    v = log((z - a)./(z - 1/conj(a))/abs(a))/2i/pi;
+end
