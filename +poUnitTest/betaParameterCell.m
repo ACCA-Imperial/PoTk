5c8adc69dd28023a12062c351deccd65ca75fe06
Everett Kropf <ehkropf@gmail.com>
2016-08-05 17:38:44 +0100

    Add beta locations for parameterisations.

--- +poUnitTest/betaParameterCell.m
+++ +poUnitTest/betaParameterCell.m
@@ -1,5 +1,6 @@
-classdef domainConn3 < poUnitTest.domainForTesting
-%poUnitTest.domainConn3 represents a 3-connected circle domain.
+classdef(Abstract) betaParameterCell
+%poUnitTest.betaParameterCell represents possible locations for the beta
+%parameter in unit tests.
 
 % Everett Kropf, 2016
 % 
@@ -18,21 +19,14 @@ classdef domainConn3 < poUnitTest.domainForTesting
 % You should have received a copy of the GNU General Public License
 % along with PoTk.  If not, see <http://www.gnu.org/licenses/>.
 
-properties
-    type = poUnitTest.domainType.Conn3
-    domainObject = unitDomain(...
-        [-0.2517+0.3129i, 0.2307-0.4667i], ...
-        [0.2377, 0.1557], ...
-        0)
-    testPoints = [
-        -0.46531-0.44082i
-        0.15044-0.062974i
-        0.44431+0.55277i
-        -0.031487+0.79067i]
-    betaLocations = struct(...
-        'origin', 0, ...
-        'inside', 0.52128+0.069971i, ...
-        'circle0', -1)
+properties(Constant)
+    defaultList = {'origin', 'inside', 'circle0'}
+end
+
+methods(Static)
+    function list = default()
+        list = poUnitTest.betaParameterCell.defaultList;
+    end
 end
 
 end
