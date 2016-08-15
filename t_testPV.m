445c77ad51390e75a654aa0c55332690f8b4ce55
Everett Kropf <ehkropf@gmail.com>
2016-08-15 17:16:49 +0100

    Update test script.

--- t_testPV.m
+++ t_testPV.m
@@ -5,7 +5,7 @@ clear
 %%
 
 test = poUnitTest.PointVortex;
-test.domainTestObject = poUnitTest.domainSimple;
+test.domainTestObject = poUnitTest.domainEntire;
 
 D = test.domainObject;
 tp = test.domainTestObject.testPoints;
@@ -14,46 +14,36 @@ pv = test.kindInstance(pvKind);
 
 
 %%
-% Check circulation around vortices.
-
-
-
-
-%%
-
-% n = numel(pv.location);
-% 
-% g0v = cell(n, 1);
-% for k = find(pv.strength(:) ~= 0)'
-%     g0v{k} = greensC0(pv.location(k), skpDomain(D));
-% end
-% 
-% W = @(z) reshape(sum(cell2mat( ...
-%     arrayfun(@(k) pv.strength(k)*g0v{k}(z(:)), find(pv.strength(:) ~= 0)', ...
-%     'uniform', false)), 2), size(z));
-
-
-%%
-
-% pf = test.primeFunctionReferenceForDomain;
-% g0 = poUnitTest.PrimeFormGreens(pf, 0, test.domainTestObject);
-% 
-% 
-% return
-
-
-%%
 
 W = potential(test.domainObject, pv);
-ref = test.generateReference(pv);
+% ref = test.generateReference(pv);
 % % test.checkAtTestPoints(ref, W)
 
-err = ref(tp) - W(tp);
-disp(err)
+% err = ref(tp) - W(tp);
+% disp(err)
 
 
+%%
+% Check circulation around vortices.
+
+N = 128;
+dt = 2*pi/N;
+eit = exp(1i*dt*(0:N-1)');
+Icirc = @(dW, c, r) 1i*dt*sum(dW(c + r*eit)*r.*eit);
+
+dr = 1e-6;
+dW = diff(W);
+cv = nan(size(pv.strength));
+for k = 1:numel(pv.location)
+    cv(k) = real(Icirc(dW, pv.location(k), dr));
+end
+
+err = pv.strength - cv;
+disp(max(abs(err)))
 
 
+%%
+% Check circulation around boundaries.
 
 
 
